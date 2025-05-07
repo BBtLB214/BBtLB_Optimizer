5b377566c38f8a276811ca8d6dec858fe48de864
BBtLB214 <firstwash@gmail.com>
2025-05-07 16:22:17 -0500

    Update projections.py

--- BBtLB_Optimizer/projections.py
+++ BBtLB_Optimizer/projections.py
@@ -1,11 +1,9 @@
-def get_player_projections(api_data, model_data, opponent_stats):
-    player_projections = {}
-    for player in api_data['players']:
-        base_proj = model_data[player['id']]['base']
-        opponent_factor = opponent_stats[player['opponent']]
-        player_projections[player['id']] = {
-            'median': base_proj['median'] * opponent_factor['multiplier'],
-            'floor': base_proj['floor'] * opponent_factor['multiplier'],
-            'ceiling': base_proj['ceiling'] * opponent_factor['multiplier'],
-        }
-    return player_projections
+import pandas as pd
+
+def generate_projections(players_df, model_data, opponent_stats):
+    df = pd.merge(players_df, opponent_stats, on="Team", how="left")
+    df["Proj_Med"] = df["Salary"] / 1000 * 2.5
+    df["Proj_Floor"] = df["Proj_Med"] * 0.8
+    df["Proj_Ceil"] = df["Proj_Med"] * 1.2
+    df["Value"] = df["Proj_Med"] / df["Salary"]
+    return df
