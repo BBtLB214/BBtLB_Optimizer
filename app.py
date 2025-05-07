66a55907f468941dfe2dff5bffdcb288f34cb770
BBtLB214 <firstwash@gmail.com>
2025-05-07 15:29:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "BBtLB_Optimizer")))
 import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
